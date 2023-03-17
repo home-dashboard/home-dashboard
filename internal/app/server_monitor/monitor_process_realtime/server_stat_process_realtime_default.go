dfbdc83f43f20506315e440bb5148dc128936828
siaikin <abc1310054026@163.com>
2023-03-18 01:42:53 +0800

    fix(server_monitor): 非 windows 构建时找不到 ignoredProcess 函数导致编译失败.

--- internal/app/server_monitor/monitor_process_realtime/server_stat_process_realtime_default.go
+++ internal/app/server_monitor/monitor_process_realtime/server_stat_process_realtime_default.go
@@ -4,6 +4,6 @@ package monitor_process_realtime
 
 import psuProc "github.com/shirou/gopsutil/v3/process"
 
-func ignoreProcess(proc *psuProc.Process) bool {
+func ignoredProcess(proc *psuProc.Process) bool {
 	return false
 }
