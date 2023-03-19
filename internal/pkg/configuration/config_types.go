ead3aebf986b6673838b8c013f2c3a14d672b3eb
siaikin <abc1310054026@163.com>
2023-03-19 15:29:37 +0800

    feat(server_monitor): 配置文件增加 wakapi 服务器的地址以及 apiKey.

--- internal/pkg/configuration/config_types.go
+++ internal/pkg/configuration/config_types.go
@@ -11,6 +11,8 @@ type ServerMonitorConfiguration struct {
 	Administrator ServerMonitorAdministratorConfiguration `json:"administrator" toml:"administrator"`
 	// 开发模式配置
 	Development ServerMonitorDevelopmentConfiguration `json:"development" toml:"development"`
+	// 第三放服务配置
+	ThirdParty ServerMonitorThirdPartyConfiguration `json:"thirdParty" toml:"thirdParty"`
 }
 
 type ServerMonitorAdministratorConfiguration struct {
@@ -31,6 +33,22 @@ type ServerMonitorDevelopmentConfiguration struct {
 		// [Access-Control-Allow-Origin]: https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
 		AllowOrigins []string `json:"allowOrigins" toml:"allowOrigins"`
 	} `json:"cors" toml:"cors"`
+}
+
+// ServerMonitorThirdPartyConfiguration 第三方服务配置
+type ServerMonitorThirdPartyConfiguration struct {
+	Wakapi ServerMonitorThirdPartyWakapiConfiguration `json:"wakapi" toml:"wakapi"`
+}
+
+// ServerMonitorThirdPartyWakapiConfiguration 第三方服务 Wakapi 的配置
+type ServerMonitorThirdPartyWakapiConfiguration struct {
+	// 是否从 Wakapi 收集数据
+	// 默认为 false
+	Enable bool `json:"enable" toml:"enable"`
+	// Wakapi 用户的 api_key
+	ApiKey string `json:"apiKey" toml:"apiKey"`
+	// Wakapi 服务的地址
+	ApiUrl string `json:"apiUrl" toml:"apiUrl"`
 }
 
 type Configuration struct {
