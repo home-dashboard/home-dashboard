e444a7f14c0b34b4faa22eb0666bdf5c3e87c2ec
siaikin <abc1310054026@163.com>
2023-04-25 23:22:19 +0800

    feat(server_monitor): 更新数据基础模型的字段权限

--- internal/app/server_monitor/monitor_model/common_model.go
+++ internal/app/server_monitor/monitor_model/common_model.go
@@ -3,8 +3,12 @@ package monitor_model
 import "gorm.io/plugin/soft_delete"
 
 type Model struct {
-	ID        uint                  `gorm:"primarykey" json:"id"`
-	CreatedAt int64                 `gorm:"autoUpdateTime:milli" json:"createdAt"`
-	UpdatedAt int64                 `gorm:"autoUpdateTime:milli" json:"updatedAt"`
+	// Id 允许读和创建, 不可写
+	ID uint `gorm:"<-:create; primarykey" json:"id"`
+	// CreatedAt 创建时间, 允许读和创建, 不可写
+	CreatedAt int64 `gorm:"<-:create; autoUpdateTime:milli" json:"createdAt"`
+	// UpdatedAt 更新时间, 可读可写
+	UpdatedAt int64 `gorm:"autoUpdateTime:milli" json:"updatedAt"`
+	// DeletedAt 删除时间, 可读可写
 	DeletedAt soft_delete.DeletedAt `gorm:"softDelete:milli" json:"deletedAt"`
 }
