daf2e341a8210dc918951fbcdf2d380d8a34475c
siaikin <abc1310054026@163.com>
2023-07-06 22:17:48 +0800

    feat(server_monitor): 现在日志接口调用时会返回输出的日志字符串.

--- internal/pkg/comfy_log/logger.go
+++ internal/pkg/comfy_log/logger.go
@@ -25,30 +25,77 @@ const (
 )
 
 // Info 用于记录流程在执行时的日志(如服务启停, 配置信息等). 在错误发生时这些信息有助于确定错误发生的原因.
-func (l *Logger) Info(format string, v ...any) {
-	_ = l.stdout.Output(2, fmt.Sprintf(joinPrefix(InfoPrefix, format, Separator), v...))
+func (l *Logger) Info(format string, v ...any) string {
+	s := l.InfoString(format, v...)
+
+	err := l.stdout.Output(2, s)
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
+	return s
+}
+
+// InfoString 与 Info 方法类似, 但不会输出日志, 而是返回日志内容.
+func (l *Logger) InfoString(format string, v ...any) string {
+	return fmt.Sprintf(joinPrefix(InfoPrefix, format, Separator), v...)
 }
 
 // Warn 用于记录流程出现异常但可能会在之后一段时间恢复(也可能不会恢复, 一般来说这将导致 Error 日志被记录)的日志. 如重试操作, 无法识别的非必要参数等.
-func (l *Logger) Warn(format string, v ...any) {
-	_ = l.stdout.Output(2, fmt.Sprintf(joinPrefix(warnPrefix, format, Separator), v...))
+func (l *Logger) Warn(format string, v ...any) string {
+	s := l.WarnString(format, v...)
+
+	err := l.stdout.Output(2, s)
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
+	return s
+}
+
+// WarnString 与 Warn 方法类似, 但不会输出日志, 而是返回日志内容.
+func (l *Logger) WarnString(format string, v ...any) string {
+	return fmt.Sprintf(joinPrefix(warnPrefix, format, Separator), v...)
 }
 
 // Error 用于记录导致流程中断的日志, 这些错误日志会提示用户需要进行某些操作以使得程序能够正常运行.
-func (l *Logger) Error(format string, v ...any) {
-	_ = l.stderr.Output(2, fmt.Sprintf(joinPrefix(ErrorPrefix, format, Separator), v...))
+func (l *Logger) Error(format string, v ...any) string {
+	s := l.ErrorString(format, v...)
+
+	err := l.stderr.Output(2, s)
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
+	return s
+}
+
+// ErrorString 与 Error 方法类似, 但不会输出日志, 而是返回日志内容.
+func (l *Logger) ErrorString(format string, v ...any) string {
+	return fmt.Sprintf(joinPrefix(ErrorPrefix, format, Separator), v...)
 }
 
 // Fatal 在输出带有 FatalPrefix 前缀的日志后调用 os.Exit(1)
 func (l *Logger) Fatal(format string, v ...any) {
-	_ = l.stderr.Output(2, fmt.Sprintf(joinPrefix(FatalPrefix, format, Separator), v...))
+	s := fmt.Sprintf(joinPrefix(FatalPrefix, format, Separator), v...)
+
+	err := l.stderr.Output(2, s)
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
 	os.Exit(1)
 }
 
 // Panic 在输出带有 PanicPrefix 前缀的日志后调用 panic()
 func (l *Logger) Panic(format string, v ...any) {
 	s := fmt.Sprintf(joinPrefix(PanicPrefix, format, Separator), v...)
-	_ = l.stderr.Output(2, s)
+
+	err := l.stderr.Output(2, s)
+	if err != nil {
+		panic(err)
+	}
+
 	panic(s)
 }
 
