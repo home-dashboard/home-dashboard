58a7349cbeec8b63a3b3ff2c66ca0afa13301c5f
siaikin <abc1310054026@163.com>
2023-10-28 13:34:49 +0800

    fix(server_monitor): 修复测试用例, 替换 GitHub Token

--- internal/pkg/overseer/fetcher/github_test.go
+++ internal/pkg/overseer/fetcher/github_test.go
@@ -4,9 +4,7 @@ import (
 	"testing"
 )
 
-func TestGitHubFetcher_Normal(t *testing.T) {
-	// 设置测试用例的参数.
-	token := "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"
+func generateFetcher(t *testing.T, token string) *GitHubFetcher {
 	currentVersion := "v1.0.0"
 	user := "home-dashboard"
 	repo := "home-dashboard"
@@ -26,12 +24,22 @@ func TestGitHubFetcher_Normal(t *testing.T) {
 		},
 	}
 
+	return fetcher
+}
+
+func TestGitHubFetcher_Normal(t *testing.T) {
+	// 设置测试用例的参数.
+	token := "GITHUB_PERSONAL_ACCESS_TOKEN"
+
+	// 创建一个新的 GitHubFetcher 实例.
+	fetcher := generateFetcher(t, token)
+
 	if err := fetcher.Init(); err != nil {
 		t.Fatalf("初始化GitHubFetcher失败: %v", err)
 	}
 
 	// 测试正常情况下的使用.
-	reader, err := fetcher.Fetch()
+	_, reader, _, err := fetcher.Fetch(true)
 	if err != nil {
 		t.Fatalf("Fetch returned an error: %v", err)
 	}
@@ -43,28 +51,15 @@ func TestGitHubFetcher_Normal(t *testing.T) {
 func TestGitHubFetcher_InvalidToken(t *testing.T) {
 	// 设置测试用例的参数.
 	token := "invalid_token"
-	currentVersion := "v1.0.0"
-	user := "home-dashboard"
-	repo := "home-dashboard"
 
-	if err := clientInitial(token); err != nil {
-		t.Fatalf("初始化GitHub客户端失败: %v", err)
-	}
-
-	// 创建一个新的 GitHubFetcher 实例.
-	fetcher := &GitHubFetcher{
-		Token:          token,
-		CurrentVersion: currentVersion,
-		User:           user,
-		Repository:     repo,
-	}
+	fetcher := generateFetcher(t, token)
 
 	if err := fetcher.Init(); err != nil {
 		t.Fatalf("初始化GitHubFetcher失败: %v", err)
 	}
 
 	// 测试当获取二进制文件时发生错误时，Fetcher 是否能够正确地处理错误并返回适当的错误信息.
-	reader, err := fetcher.Fetch()
+	_, reader, _, err := fetcher.Fetch(true)
 	if err == nil {
 		t.Fatal("Fetch did not return an error")
 	}
@@ -75,32 +70,16 @@ func TestGitHubFetcher_InvalidToken(t *testing.T) {
 
 func TestGitHubFetcher_Update(t *testing.T) {
 	// 设置测试用例的参数.
-	token := "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"
-	currentVersion := "v1.0.0"
-	user := "home-dashboard"
-	repo := "home-dashboard"
+	token := "GITHUB_PERSONAL_ACCESS_TOKEN"
 
-	if err := clientInitial(token); err != nil {
-		t.Fatalf("初始化GitHub客户端失败: %v", err)
-	}
-
-	// 创建一个新的 GitHubFetcher 实例.
-	fetcher := &GitHubFetcher{
-		Token:          token,
-		CurrentVersion: currentVersion,
-		User:           user,
-		Repository:     repo,
-		OnProgress: func(written, total uint64) {
-			t.Logf("下载进度: %d/%d", written, total)
-		},
-	}
+	fetcher := generateFetcher(t, token)
 
 	if err := fetcher.Init(); err != nil {
 		t.Fatalf("初始化GitHubFetcher失败: %v", err)
 	}
 
 	// 测试当获取二进制文件的频率过低时，Fetcher 是否能够及时获取更新的二进制文件，以确保应用程序始终是最新的版本.
-	reader, err := fetcher.Fetch()
+	_, reader, _, err := fetcher.Fetch(true)
 	if err != nil {
 		t.Fatalf("Fetch returned an error: %v", err)
 	}
